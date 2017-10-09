3370f7d30fbb6e542966a1109feb3b5ecae50581
mohamed <mohamed.mohamed.mhiri@gmail.com>
2017-10-09 10:34:10 +0100

    vuesjs component #3

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 Vue.component('task-list', {
-    template: `<li><slot></slot></li>`
+    template: `<ul>
+                <li v-for="task in tasks" v-text="task.description"></li>
+               </ul>`
 })
+let tasks: [
+    { desctiption: 'Wake Up', done: true },
+    { desctiption: 'Coding', done: false },
+    { desctiption: 'practising sport', done: false },
+    { desctiption: 'Helping someone', done: false },
+    { desctiption: 'Learning VueJs', done: true },
+    { desctiption: 'Pushing code on github', done: true },
+    { desctiption: 'Taking dinner', done: false },
 
+]
 new Vue({
-    el: '#root'
+    el: '#root',
+    data: tasks
 })
