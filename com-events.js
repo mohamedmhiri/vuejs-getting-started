ec31a2b5d39ff1434067d037102a881f09d4bb2a
mohamed <mohamed.mohamed.mhiri@gmail.com>
2017-10-11 16:25:30 +0100

    components communicating events communication #2

--- com-events.js
+++ com-events.js
@@ -3,23 +3,27 @@ window.Event = new Vue({})
 
 Vue.component('coupon', {
     template: `
-        <input type="text" placeholder="Enter your coupon code" @blur="onCouponApplied">
+        <input type="text" placeholder="Enter your coupon code" @blur="onCouponApplied" @click.ctrl="hideHeader">
     `,
     methods: {
         onCouponApplied() {
             Event.$emit('applied')
+        },
+        hideHeader () {
+            Event.$emit('hide')
         }
     }
 })
 
 Vue.component('moom-header', {
     template: `<h1 v-show="show"><slot></slot></h1>`,
-    methods: {
-        onCouponApplied () {
-            Event.$on('applied', () => {
-                this.show = true
-            })
-        }
+    created () {
+        Event.$on('applied', () => {
+            this.show = true
+        })
+        Event.$on('hide', () => {
+            this.show = false
+        })
     },
     data () {
         return {
